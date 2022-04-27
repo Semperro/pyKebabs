e09aa7de7bea8dad6cd369521309ca4a833825df
Semperro <mindbrix@gmx.at>
2022-04-27 10:37:45 +0200

    Added Package Support

---
+++ pybabs/__init__.py
@@ -0,0 +1,1 @@
+from .pybabs import Pybabs
---
+++ pybabs/pybabs.py
@@ -0,0 +1,8 @@
+class Pybabs:
+
+    def __init__(self,fname):
+        self.fname = fname
+
+    def test(self):
+        print("Test Suceeded")
+        
